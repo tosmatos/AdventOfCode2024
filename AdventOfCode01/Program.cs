97b74446590f4ca8985609b344a155a941a691c9
Matos <18725330+tosmatos@users.noreply.github.com>
2024-12-01 12:04:47 +0100

    Solution for Day 1 part 2

--- AdventOfCode01/Program.cs
+++ AdventOfCode01/Program.cs
@@ -2,6 +2,7 @@
 List<int> list1 = new();
 List<int> list2 = new();
 List<int> differenceList = new();
+List<int> similarityList = new();
 
 foreach (string line in inputLines)
 {
@@ -11,6 +12,16 @@ foreach (string line in inputLines)
 
 int numElements = list1.Count;
 
+// Loop for part 2 comes first cause part 1 destroys the list
+for (int i = 0; i < numElements; i++)
+{
+    int number = list1[i];
+    int numberCount = list2.Count(inList2 => inList2 == number);
+
+    similarityList.Add(number * numberCount);
+}
+
+// Loop for part 1
 for (int i = 0; i < numElements; i++)
 {
     int min1 = list1.Min();
@@ -23,3 +34,6 @@ for (int i = 0; i < numElements; i++)
 
 int totalDifference = differenceList.Sum();
 Console.WriteLine("Total difference : " + totalDifference);
+
+int totalSimilarity = similarityList.Sum();
+Console.WriteLine("Total similarity score : " + totalSimilarity);
