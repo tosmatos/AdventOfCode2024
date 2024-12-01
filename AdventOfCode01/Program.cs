1a1eb03f94da31dfbfb4c6e2af69fee89c269977
Matos <18725330+tosmatos@users.noreply.github.com>
2024-12-01 11:54:39 +0100

    Initial commit + solution for day 1 part 1

---
+++ AdventOfCode01/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] inputLines = File.ReadAllLines("input.txt");
+List<int> list1 = new();
+List<int> list2 = new();
+List<int> differenceList = new();
+
+foreach (string line in inputLines)
+{
+    list1.Add(int.Parse(line.Split("   ")[0]));
+    list2.Add(int.Parse(line.Split("   ")[1]));
+}
+
+int numElements = list1.Count;
+
+for (int i = 0; i < numElements; i++)
+{
+    int min1 = list1.Min();
+    int min2 = list2.Min();
+
+    differenceList.Add(Math.Abs(min1 - min2));
+    list1.Remove(min1);
+    list2.Remove(min2);
+}
+
+int totalDifference = differenceList.Sum();
+Console.WriteLine("Total difference : " + totalDifference);
