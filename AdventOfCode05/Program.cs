147a4b415e63dfa6a02dfec5185176f751084140
Matos <18725330+tosmatos@users.noreply.github.com>
2024-12-05 16:48:04 +0100

    Solution start for day 5 part 1

---
+++ AdventOfCode05/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] inputLines = File.ReadAllLines("input.txt");
+
+List<(int page1, int page2)> pageOrderingRules = new();
+List<List<int>> updates = new();
+
+// Parse input
+for (int i = 0; i < inputLines.Length; i++)
+{
+	string line = inputLines[i];
+
+	if (line.Contains('|'))
+	{
+		(int page1, int page2) pageOrderingRule = (int.Parse(line.Split('|')[0]), int.Parse(line.Split('|')[1]));
+		pageOrderingRules.Add(pageOrderingRule);
+	}
+	else if (line.Contains(','))
+	{
+		string[] values = line.Split(",");
+		List<int> update = new();
+		foreach (string value in values)
+		{
+			update.Add(int.Parse(value));
+		}
+		updates.Add(update);
+	}
+}
+
+Console.WriteLine("Hello !");
